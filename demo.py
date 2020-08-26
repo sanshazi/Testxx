e68144b553b70f160598c8fc16a3102a308fa0c5
mali123 <cxm15356486156@126.com>
2020-08-26 22:46:10 +0800

    第二次提交

--- demo.py
+++ demo.py
@@ -1,1 +1,52 @@
-print("hello worhhhld")
+# print("hello worldaaa",end="")
+# print(233)
+# print(2.22)
+# print(True,False)
+# print(None)
+# print(())
+# print([])
+# print({})
+# print("大傻子 + 傻媳妇 = 三傻子" )
+# print("大傻子 + 123 = 三傻子" )
+
+# print(12/3+222)
+
+
+# name="大傻子"
+# age="18"
+# sex="女"
+
+# print("姓名:"+ name + "年龄:" + age + "性别:" + sex)
+# print("姓名:", name ,"年龄:" , age , "性别:" , sex)
+
+
+# print("年龄:", age)
+# print("性别:", sex)1
+
+# name=input("请输入您的姓名：")
+# print("姓名:", name)
+
+# a=int(input("请输入第一个数字:"))
+# b=int(input("请输入第二个数字:"))
+# print(a+b)
+
+# a=input("请输入第一个数字:")
+# b=input("请输入第二个数字:")
+
+# print("计算结果：",float(a)+float(b))
+
+
+# a="123"
+# print(type(a))
+# a=len("哈哈哈哈哈哈哈哈哈哈")
+# print(a)
+
+print("0:表示偶数; 1:表示奇数")
+a=input("请输入数字：")
+print(len(a)%2)
+
+# print("0：表示偶数，1：表示奇数")
+# a=input("请输入数字：")
+# print(len(a)%2)
+
+
